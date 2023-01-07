bc78ffad44c3f33fe696a1fab5c951344659f42d
moosamahroof <mahroofanchukandan@gmail.com>
2023-01-07 09:25:02 +0530

    Initial commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,51 @@
+class RoomDis {
+  static const String description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)";
+}
+
+List<Map> RoomData = [
+  {
+    'id' : 0,
+    'image' : 'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG90ZWx8ZW58MHx8MHx8&w=1000&q=80',
+    'title' : 'Awesome room near infopark',
+    'location' : 'Kochi, Kerala',
+    'price' :  12,
+    'rating' : 4.0,
+    'review' : 220,
+  },
+  {
+    'id' : 1,
+    'image' : 'https://pix10.agoda.net/hotelImages/109/109651/109651_14052314390019548951.jpg?ca=2&ce=1&s=1024x768',
+    'title' : 'luxury appartments',
+    'location' : 'Kakkanad, Kerala',
+    'price' :  40,
+    'rating' : 4.5,
+    'review' : 115,
+  },
+  {
+    'id' : 2,
+    'image' : 'https://www.tajhotels.com/content/dam/vivanta/hotels/VBT-Gomti_Nagar_Lucknow/images/gallery/Pool.jpg/_jcr_content/renditions/cq5dam.web.756.756.jpeg',
+    'title' : 'four star pool villa',
+    'location' : 'Palarivattom, Kerala',
+    'price' :  34,
+    'rating' : 5,
+    'review' : 85,
+  },
+  {
+    'id' : 3,
+    'image' : 'https://r1imghtlak.mmtcdn.com/52689fcaa63211ecad380a58a9feac02.jpg?&output-quality=75&downsize=910:612&crop=910:612;4,0&output-format=jpg',
+    'title' : 'Beautiful room near lulu',
+    'location' : 'LuluMall, Kerala',
+    'price' :  20,
+    'rating' : 4.0,
+    'review' : 32,
+  },
+  {
+    'id' : 4,
+    'image' : 'https://r1imghtlak.mmtcdn.com/1a9d3de8a63311ec9b910a58a9feac02.jpg',
+    'title' : 'Super conditioned room',
+    'location' : 'Vytila, Kerala',
+    'price' :  10,
+    'rating' : 3.0,
+    'review' : 42,
+  },
+];
